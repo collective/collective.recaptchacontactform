d3e62a18cc2a15ddb305c65ef0279613b29e13f6
Victor Fernandez de Alba <sneridagh@gmail.com>
2018-05-28 15:24:05 +0200

    Adapt code to use proper z3c.form extenders in Plone 5.1, add a detection for older Plone 5 flavours

--- src/collective/recaptchacontactform/__init__.py
+++ src/collective/recaptchacontactform/__init__.py
@@ -9,47 +9,47 @@ from zope.i18nmessageid import MessageFactory
 from zope.component import getMultiAdapter
 from z3c.form import button
 
+import inspect
 import Products.CMFPlone.browser.contact_info
 
 _ = MessageFactory('collective.recaptchacontactform')
 
+if AutoExtensibleForm not in inspect.getmro(ContactForm):
+    # monkey patch ContactForm class since it is not extensible by default
+    # see https://github.com/plone/Products.CMFPlone/issues/1879 for details
+    class PatchedContactForm(AutoExtensibleForm, ContactForm):
 
-# monkey patch ContactForm class since it is not extensible by default
-# see https://github.com/plone/Products.CMFPlone/issues/1879 for details
-class PatchedContactForm(AutoExtensibleForm, ContactForm):
+        ignoreContext = True
+        schema = IContactForm
 
-    ignoreContext = True
-    schema = IContactForm
+        @button.buttonAndHandler(_(u'label_send', default='Send'), name='send')
+        def handle_send(self, action):
+            data, errors = self.extractData()
 
-    @button.buttonAndHandler(_(u'label_send', default='Send'), name='send')
-    def handle_send(self, action):
-        data, errors = self.extractData()
-
-        # recaptcha validation start
-        captcha = getMultiAdapter(
-            (aq_inner(self.context), self.request),
-            name='recaptcha'
-        )
-        if not captcha.verify():
-            api.portal.show_message(
-                'Please verify that you are not a robot',
-                self.request,
-                type=u'error'
+            # recaptcha validation start
+            captcha = getMultiAdapter(
+                (aq_inner(self.context), self.request),
+                name='recaptcha'
             )
-            return
-        # recaptcha validation end
+            if not captcha.verify():
+                api.portal.show_message(
+                    'Please verify that you are not a robot',
+                    self.request,
+                    type=u'error'
+                )
+                return
+            # recaptcha validation end
 
-        if errors:
-            api.portal.show_message(
-                self.formErrorsMessage,
-                self.request,
-                type=u'error'
-            )
-            return
+            if errors:
+                api.portal.show_message(
+                    self.formErrorsMessage,
+                    self.request,
+                    type=u'error'
+                )
+                return
 
-        self.send_message(data)
-        self.send_feedback()
-        self.success = True
+            self.send_message(data)
+            self.send_feedback()
+            self.success = True
 
-
-Products.CMFPlone.browser.contact_info.ContactForm = PatchedContactForm
+    Products.CMFPlone.browser.contact_info.ContactForm = PatchedContactForm
