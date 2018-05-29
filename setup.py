f469910c88f3e894eccf710d5af7198c3446c9e4
Timo Stollenwerk <tisto@plone.org>
2018-05-29 15:07:10 +0200

    Back to development: 1.0.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.recaptchacontactform',
-    version='1.0.0',
+    version='1.0.1-dev.0',
     description='Protect the Plone contact form with ReCaptcha 2 from spam.',
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
